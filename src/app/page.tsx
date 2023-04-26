5efee3ed3453e23a10429e92cd56cae857be83e6
Jason <cmjj8898212032@gmail.com>
2023-04-26 18:58:26 +0800

    feat: add basic structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,49 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import styled from "styled-components";
+// import styles from "./page.module.css";
+
+import { Fonts, Colors } from "@/themes";
+
+import IconSvg from "public/images/icon-arrow.svg";
+
+const SvgWrapper = styled.div`
+  svg path {
+    stroke: red;
+  }
+`;
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div>
+        <div>
+          <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+          </div>
+        </div>
+
+        <div>
+          <hr />
+          <SvgWrapper>
+            <IconSvg />
+          </SvgWrapper>
+        </div>
+
+        <div>
+          <h1>-- years</h1>
+          <h1>-- months</h1>
+          <h1>-- days</h1>
+        </div>
+      </div>
+    </main>
+  );
 }
